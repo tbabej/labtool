ff7a0f447f0788be90d9fa11a3609ea36fa8e6bb
Tomas Babej <tbabej@redhat.com>
2013-07-03 11:45:14 +0200

    Refactor apply-patch script

--- ipa-fun-apply-patch.sh
+++ ipa-fun-apply-patch.sh
@@ -6,7 +6,7 @@ source $DIR/config.sh
 # Validate patch name
 if [[ $1 == "" ]]
 then
-  echo "Usage: $0 patch_number [destination]"
+  echo "Usage: $0 patch_number"
   exit 1
 else
   MATCHES=`$DIR/ipa-fun-get-patch-name.sh $1`
@@ -19,25 +19,14 @@ else
   fi
 fi
 
-# Check if local was given as option
-if [[ $2 == "local" ]]
-then
-  IPA_DIR=~/dev/freeipa
-fi
+# Exception exist set now, because ipa-fun-get-patch-name
+# does not always return 0
+set -e
 
 pushd $IPA_DIR
 
 # Apply the patch
 echo "Applying the patch $PATCH."
-OUTPUT=`git am $PATCH 2>&1 | grep "Patch format detection failed."`
-
-if [[ `echo $OUTPUT | grep "Patch format detection failed."` != '' ]]
-then
-  echo 'Patch format detection failed, falling back to git apply utility.'
-  set -e
-  git apply $PATCH
-  git add --all
-  git commit -a -m "Testing patch $PATCH"
-fi
+git am $PATCH 2>&1
 
 popd
