126aca40c3149e879d1ef1e2e158972c17b897cd
Tomas Babej <tbabej@redhat.com>
2013-07-03 12:55:02 +0200

    Remove destination support from rpms install script

--- ipa-fun-install-rpms.sh
+++ ipa-fun-install-rpms.sh
@@ -18,21 +18,9 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/config.sh
 
-# Set DIST_DIR if given via command line
-if [[ ! $1 == "" ]]
-then
-  if [[ ! -d $WORKING_DIR/$1 ]]
-  then
-    echo "$WORKING_DIR/$1 does not exist"
-    exit 1
-  fi
-
-  DIST_DIR=$WORKING_DIR/$1/dist
-fi
-
 pushd $DIST_DIR
 
-sudo yum localinstall freeipa-* -y
+sudo yum localinstall freeipa-* --enablerepo=updates-testing -y
 
 popd
 
