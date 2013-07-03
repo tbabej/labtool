e0b0804a6f194add34e17cfcedcb40c77b321827
Tomas Babej <tbabej@redhat.com>
2013-07-03 11:47:10 +0200

    Remove custom directory support from check-rpms

--- ipa-fun-check-rpms.sh
+++ ipa-fun-check-rpms.sh
@@ -3,18 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/config.sh
 
-# Set DIST_DIR if given via command line
-if [[ ! $1 == "" ]]
-then
-  if [[ ! -d $WORKING_DIR/$1 ]]
-  then
-    echo "$WORKING_DIR/$1 does not exist"
-    exit 1
-  fi
-
-  DIST_DIR=$WORKING_DIR/$1/dist
-fi
-
 if [[ `ls $DIST_DIR | wc -l` -eq 0 ]]
 then
   echo "There are no rpms in $DIST_DIR"
