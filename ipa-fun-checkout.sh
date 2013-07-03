0a910fbdce6bcf3afc869238a75385112697260d
Tomas Babej <tbabej@redhat.com>
2013-07-03 12:50:43 +0200

    Remove destination support from checkout script

--- ipa-fun-checkout.sh
+++ ipa-fun-checkout.sh
@@ -10,14 +10,8 @@ source $DIR/config.sh
 # TODO: support just pieces of branch name
 if [[ $1 == "" ]]
 then
-  echo "Usage: $0 branch [destination]"
+  echo "Usage: $0 branch "
   exit 1
-fi
-
-# Check if destination was given as option
-if [[ $2 == "local" ]]
-then
-  IPA_DIR=~/dev/freeipa
 fi
 
 pushd $IPA_DIR
