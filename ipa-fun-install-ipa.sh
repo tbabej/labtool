e9c17f268077aeb5cb90fdb35ab23302c950a988
Tomas Babej <tbabej@redhat.com>
2013-08-25 02:05:02 +0200

    Edit resolv.conf before installing IPA

--- ipa-fun-install-ipa.sh
+++ ipa-fun-install-ipa.sh
@@ -9,9 +9,9 @@ source $DIR/config.sh
 # Install the dependencies
 sudo yum install bind-dyndb-ldap bash-completion -y
 
-# Install the IPA server
-sudo ipa-server-install -U -r $DOMAIN  -p $PASSWORD  -a $PASSWORD --setup-dns --no-forwarders
-
 # Add localhost as a name server and disable rewrites
 sudo sed -i "1inameserver 127.0.0.1" /etc/resolv.conf
 sudo chattr +i /etc/resolv.conf
+
+# Install the IPA server
+sudo ipa-server-install -U -r $DOMAIN  -p $PASSWORD  -a $PASSWORD --setup-dns --no-forwarders
