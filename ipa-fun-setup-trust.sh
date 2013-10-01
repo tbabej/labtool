c23a451194b1d6affb1c7a16ebbf7ed94543ccd8
Tomas Babej <tbabej@redhat.com>
2013-10-01 14:20:43 +0200

    setup-trust: Move dnszone addition before time synchronization

--- ipa-fun-setup-trust.sh
+++ ipa-fun-setup-trust.sh
@@ -13,6 +13,12 @@ then
   NETBIOS=DOM`echo $1 | cut -d- -f2`
 fi
 
+# Configure DNS only if on master
+if [[ $1 == '' ]]
+then
+  ipa dnszone-add $AD_DOMAIN --name-server=advm.$AD_DOMAIN --admin-email="hostmaster@$AD_DOMAIN.com" --force --forwarder=$AD_IP --forward-policy=only --ip-address=$AD_IP
+fi
+
 # Fix the time issues
 sudo systemctl stop ntpd
 sudo systemctl disable ntpd
@@ -23,12 +29,6 @@ echo $PASSWORD | kinit admin
 
 # Install support for trusts
 sudo ipa-adtrust-install --netbios-name=$NETBIOS -a $PASSWORD --add-sids -U
-
-# Configure DNS only if on master
-if [[ $1 == '' ]]
-then
-  ipa dnszone-add $AD_DOMAIN --name-server=advm.$AD_DOMAIN --admin-email="hostmaster@$AD_DOMAIN.com" --force --forwarder=$AD_IP --forward-policy=only --ip-address=$AD_IP
-fi
 
 # Make sure that resolv.conf contains localhost
 if [[ `cat /etc/resolv.conf | grep $IP` == '' ]]
