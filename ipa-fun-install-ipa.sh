47c592921e2dcf681993947e94ef72aa31e48180
Tomas Babej <tomasbabej@gmail.com>
2013-11-12 13:55:09 +0100

    install-ipa: Set resolv.conf mutable before editing it

--- ipa-fun-install-ipa.sh
+++ ipa-fun-install-ipa.sh
@@ -7,8 +7,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/config.sh
 
 # Add localhost as a name server, installer needs hostname resolvable
-sudo sed -i "1inameserver 127.0.0.1" /etc/resolv.conf
 sudo chattr -i /etc/resolv.conf
+sudo sed -i "1inameserver 127.0.0.1" /etc/resolv.conf
 
 # Install the IPA server
 sudo ipa-server-install -U -r $DOMAIN  -p $PASSWORD  -a $PASSWORD --setup-dns --no-forwarders
