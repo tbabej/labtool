c6d47e3e04bc54a80e6069af65241ab6c7ebda07
Petr Spacek <pspacek@redhat.com>
2016-01-21 17:30:21 +0100

    remove non-functional and unused code from printer.py

--- printer.py
+++ printer.py
@@ -10,23 +10,15 @@ class Printer():
 
     def __init__(self):
         self.prefix = ''
-        self.silence = False
 
     def __call__(self, text):
-        if not self.silence:
-            print(self.prefix + text)
+        print(self.prefix + text)
 
     def tab(self, space=' '):
         self.prefix = self.prefix + 4 * space
 
     def untab(self):
         self.prefix = self.prefix[:-4]
-
-    def silence(self):
-        self.silence = True
-
-    def talk(self):
-        self.silence = False
 
     def debug(self, msg):
         if locals.DEBUG:
